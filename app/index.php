66e4abdc0d01d6c81e7e6f7714a879c73dbc9f78
Administrator <dankoshevoy2001@gmail.com>
2024-01-08 21:53:04 +0300

    up

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Официальный сайт кафедры неврологии ФГБУ ДПО "Центральная государственная медицинская академия" УД Президента РФ</title>
     <!-- Link Swiper's CSS-->
     <link rel="icon" href="frontend/images/Flag_of_Russia.svg" type="image/x-icon"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
