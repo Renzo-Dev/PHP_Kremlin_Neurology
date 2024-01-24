27eb8059c40a73e02c303a157ccc134516f39e4b
Administrator <renzo@Dan-PC>
2024-01-24 22:29:20 +0300

    update

--- app/index.php
+++ app/index.php
@@ -1,29 +1,17 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Официальный сайт кафедры неврологии ФГБУ ДПО "Центральная государственная медицинская академия" УД Президента
-        РФ</title>
-    <!-- Link Swiper's CSS-->
-    <link rel="icon" href="frontend/images/Flag_of_Russia.svg" type="image/x-icon"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="frontend/styles/tNavPSwiper.css">
-    <link rel="stylesheet" href="frontend/styles/topBar.css">
-    <link rel="stylesheet" href="frontend/styles/main.css">
-    <link rel="stylesheet" href="frontend/styles/footer.css">
-</head>
-<body>
-<header>
-    <?php include("frontend/html/templates/topBar.html"); ?>
-</header>
-<main>
-    <?php include("frontend/html/main.html"); ?>
-</main>
-<?php include("frontend/html/templates/footer.html"); ?>
+<?php
+$path = "frontend/html/main.html";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    if (isset($_GET['library'])) {
+        $path = "frontend/html/library.html";
+    }
+}
+
+include("frontend/html/templates/topBar.html");
+include($path);
+include("frontend/html/templates/footer.html");
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="frontend/scripts/swiper.js"></script>
 <script src="frontend/scripts/topPanelNavigation.js"></script>
-</body>
-</html>
