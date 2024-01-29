e5ca528d30fed57fd941f61a472a11e28158f852
Administrator <renzo@Dan-PC>
2024-01-29 17:26:12 +0300

    update

--- app/backend/AccessLibrary.php
+++ app/backend/AccessLibrary.php
@@ -4,6 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $jsonData = file_get_contents("php://input");
     $data = json_decode($jsonData, true);
     if (isset($data["password"]) && $data["password"] === $password) {
+        // ПЕРЕНАПРАВЛЯЕМ НА ПРИВАТНУЮ БИБЛИОТЕКУ
+        // ИЛИ ОТПРАВЛЯЕМ ШАБЛОН С НЕЙ
         echo "200";
     }else {
         echo "401";
